3fc18acae70a2db00397d34fbc29a2aa713b78a7
Sai Manoj <saimanoj2826@gmail.com>
2024-05-17 13:16:21 +0530

    Grouping test with describe

--- src/02_TDD/Greet1.test.js
+++ src/02_TDD/Greet1.test.js
@@ -5,6 +5,23 @@
 import { render, screen } from '@testing-library/react';
 import Greet1 from './Greet1';
 
+// Grouping Tests
+describe('Greet', () => {
+  test('renders hello', () => {
+    render(<Greet1 />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test('renders with a name', () => {
+    render(<Greet1 name='Manoj' />);
+    // const textElement = screen.getByText(/hello manoj/i);
+    const textElement = screen.getByText(/hello.*manoj/i);
+    expect(textElement).toBeInTheDocument();
+  });
+});
+
+/*
 test('Greet renders hello', () => {
   render(<Greet1 />);
   const textElement = screen.getByText(/hello/i);
@@ -17,3 +34,4 @@ test('Greet renders with a name', () => {
   const textElement = screen.getByText(/hello.*manoj/i);
   expect(textElement).toBeInTheDocument();
 });
+*/
