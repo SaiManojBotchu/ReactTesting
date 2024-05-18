c1c2cb1e6e60749c7638c5fe530a93d74805d20e
Sai Manoj <saimanoj2826@gmail.com>
2024-05-18 16:28:39 +0530

    debugging and logRoles

--- src/07_findBy/List1.test.js
+++ src/07_findBy/List1.test.js
@@ -1,16 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, logRoles } from '@testing-library/react';
 import List1 from './List1';
 
 const skills = ['one', 'two', 'three'];
 
 describe('Skills', () => {
   test('start learing button is eventually displayed', async () => {
-    render(<List1 skills={skills} />);
+    const view = render(<List1 skills={skills} />);
+    logRoles(view.container);
+    // screen.debug();
     const learningButton = await screen.findByRole(
       'button',
       { name: 'Start Learning' },
       { timeout: 2000 }
     );
+    // screen.debug();
     expect(learningButton).toBeInTheDocument();
   });
 });
